b667b01aa9db89a5801bfd6f3393f196bb052a29
FernandoSC1 <77336736+FernandoSC1@users.noreply.github.com>
2025-08-03 22:59:59 -0300

    Update FormularioIRAS.js

--- src/pages/FormularioIRAS.js
+++ src/pages/FormularioIRAS.js
@@ -101,7 +101,7 @@ const FormularioIRAS = ({ setCurrentPage }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/investigacao-iras', formData);
+      const response = await axios.post(`${API_URL}/api/investigacao-iras`, formData);
       console.log('Formulário de Investigação de IRAS enviado com sucesso!', response.data);
       alert('Formulário de IRAS salvo com sucesso!');
       setCurrentPage('pacienteList');
